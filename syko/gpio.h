bffc7b8c07bca5e34a98a29f89584f581f75426e
bartekle <40119558+bartekle@users.noreply.github.com>
2018-06-10 22:58:51 +0200

    Add files via upload

--- syko/gpio.h
+++ syko/gpio.h
@@ -5,6 +5,8 @@ void loadGPIOchanges(char *file, unsigned int cycle_start, unsigned int cycle_en
 void saveGPIOchanges(char *file, unsigned int cycle_start, unsigned int cycle_end);
 void dumpGPIOchangesIN(void);
 unsigned char getGPIO(int num, unsigned int counter);
-
+void saveOC2changes(char *file,CounterType step,int state);
+void saveChanges(CounterType from_step);
+void saveGPIO(char *file, CounterType simulation_end);
 
 #endif //__GPIO_H__
